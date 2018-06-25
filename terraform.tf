e721fc60f2b8fb2f093202b399473ea146c9ff1d
TheLocust3 <jake.kinsella@gmail.com>
2018-06-25 12:00:48 -0400

    Update terraform to use common resources

--- terraform.tf
+++ terraform.tf
@@ -6,8 +6,12 @@ variable "name" {
   default = "candidatexyzapp"
 }
 
-variable "username" {
-  default = "candidatexyz"
+variable "database_name" {
+  default = "api"
+}
+
+variable "common_name" {
+  default = "api"
 }
 
 variable "key" {
@@ -38,86 +42,20 @@ data "aws_vpc" "default" {
 }
 
 data "aws_acm_certificate" "certificate" {
-  domain = "demo.candidatexyz.com"
+  domain = "*.candidatexyz.com"
 }
 
-resource "random_id" "database_password" {
-  keepers = {
-    password = "${var.username}"
-  }
-
-  byte_length = 16
+data "aws_db_instance" "database" {
+  db_instance_identifier = "${var.database_name}"
 }
 
-resource "random_id" "random" {
-  keepers = {
-    password = "${var.name}" // Using this for snapshot names
-  }
-
-  byte_length = 8
+resource "aws_s3_bucket" "bucket" {
+  bucket = "candidatexyz-${var.name}"
+  acl    = "aws-exec-read"
 }
 
-resource "aws_security_group" "security_group" {
-  name        = "${var.name} security group"
-  description = "Managed by Terraform"
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 443
-    to_port     = 443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-resource "aws_security_group" "db_security_group" {
-  name        = "${var.name} rds security group"
-  description = "Managed by Terraform"
-
-  ingress {
-    from_port       = 0
-    to_port         = 0
-    protocol        = "-1"
-    security_groups = ["${aws_security_group.security_group.id}"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-data "aws_iam_policy_document" "ec2-role" {
-  statement {
-    actions = ["sts:AssumeRole"]
-
-    principals {
-      type        = "Service"
-      identifiers = ["ec2.amazonaws.com"]
-    }
-  }
+data "aws_security_group" "security_group" {
+  name = "${var.common_name}-ec2"
 }
 
 resource "aws_iam_role" "ec2-role" {
@@ -161,7 +99,7 @@ resource "aws_launch_configuration" "launch" {
   name                 = "${var.name}-config"
   image_id             = "${data.aws_ami.image.id}"
   instance_type        = "t2.micro"
-  security_groups      = ["${aws_security_group.security_group.id}"]
+  security_groups      = ["${data.aws_security_group.security_group.id}"]
   iam_instance_profile = "${aws_iam_instance_profile.ec2-profile.name}"
   key_name             = "${var.key}"
 
@@ -174,7 +112,7 @@ resource "aws_lb" "load_balancer" {
   name               = "${var.name}-lb"
   internal           = false
   load_balancer_type = "application"
-  security_groups    = ["${aws_security_group.security_group.id}"]
+  security_groups    = ["${data.aws_security_group.security_group.id}"]
   subnets            = ["${data.aws_subnet.east1.id}", "${data.aws_subnet.east2.id}"]
 }
 
@@ -229,22 +167,6 @@ resource "aws_autoscaling_group" "autoscaling" {
   }
 }
 
-resource "aws_db_instance" "db_instance" {
-  identifier             = "${var.name}"
-  allocated_storage      = "5"
-  storage_type           = "gp2"
-  engine                 = "postgres"
-  engine_version         = "9.6.6"
-  instance_class         = "db.t2.micro"
-  vpc_security_group_ids = ["${aws_security_group.db_security_group.id}"]
-
-  final_snapshot_identifier = "${var.name}-final-snapshot-${random_id.database_password.hex}"
-
-  name     = "${var.name}"
-  username = "${var.username}"
-  password = "${random_id.database_password.hex}"
-}
-
 resource "aws_codedeploy_app" "application" {
   name = "${var.name}"
 }
@@ -259,18 +181,6 @@ resource "aws_codedeploy_deployment_group" "deployment" {
     deployment_option = "WITHOUT_TRAFFIC_CONTROL"
     deployment_type   = "IN_PLACE"
   }
-}
-
-output "database_username" {
-  value = "${aws_db_instance.db_instance.username}"
-}
-
-output "database_address" {
-  value = "${aws_db_instance.db_instance.address}"
-}
-
-output "database_password" {
-  value = "${aws_db_instance.db_instance.password}"
 }
 
 output "dns_name" {
