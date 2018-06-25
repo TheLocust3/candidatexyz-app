1a5f63e50e326908c3ad94abea7ee97c731c27a2
TheLocust3 <jake.kinsella@gmail.com>
2018-06-25 15:51:39 -0400

    New favicon

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root to: 'root#index'
 
-  get '*path', to: 'root#index'
+  get '*path', to: 'root#index', :constraints => lambda{|req| req.path !~ /\.(min.css|min.js|jpeg|png|ico)$/ }
 end
