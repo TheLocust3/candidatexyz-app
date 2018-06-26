70e27a8db4b96ebac0cbed05d2f123361939d926
TheLocust3 <jake.kinsella@gmail.com>
2018-06-26 10:20:02 -0400

    Finish at a glance website overview

--- app/javascript/components/components/website/AnalyticsOverview.jsx
+++ app/javascript/components/components/website/AnalyticsOverview.jsx
@@ -1,14 +1,23 @@
+import _ from 'lodash';
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 import Text from '../common/Text';
 
 export default class Website extends React.Component {
 
     render() {
+        let today = _.filter(this.props.analyticEntries, (analyticEntry) => { return moment(analyticEntry.createdAt) > moment().subtract(24, 'hours') });
+        let month = _.filter(this.props.analyticEntries, (analyticEntry) => { return moment(analyticEntry.createdAt) > moment().subtract(31, 'days') });
+
         return (
             <div className='content'>
                 <Text type='body2'>Total site views: {this.props.analyticEntries.length}</Text>
+
+                <Text type='body2'>Site views over last 24 hours: {today.length}</Text>
+
+                <Text type='body2'>Site views over last 31 days: {month.length}</Text>
             </div>
         );
     }
