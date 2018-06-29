029ff6cfc6c075da44d2d7bb5121cc67d0c34d1d
TheLocust3 <jake.kinsella@gmail.com>
2018-06-29 11:11:13 -0400

    Remove seeds

--- db/seeds.rb
+++
@@ -1,2 +0,0 @@
-User.create(email: 'jake@candidatexyz.com', password: 'password', first_name: 'Jake', last_name: 'Kinsella', admin: true, superuser: true)
-User.create(email: 'test@candidatexyz.com', password: 'password', first_name: 'Test', last_name: 'Test', admin: false)
