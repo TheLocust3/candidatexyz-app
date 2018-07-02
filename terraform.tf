3d6eb17679e365c30d75318a1b9f6516b9f7763e
TheLocust3 <jake.kinsella@gmail.com>
2018-07-02 16:13:33 -0400

    Blue green deployment note

--- terraform.tf
+++ terraform.tf
@@ -194,6 +194,34 @@ resource "aws_codedeploy_deployment_group" "deployment" {
     deployment_option = "WITHOUT_TRAFFIC_CONTROL"
     deployment_type   = "IN_PLACE"
   }
+
+  // Terraform fails to setup Blue Green deployment so currently just set it up in AWS console
+  /*deployment_style {
+    deployment_option = "WITH_TRAFFIC_CONTROL"
+    deployment_type   = "BLUE_GREEN"
+  }
+
+  load_balancer_info {
+    target_group_info {
+      name = "${aws_lb_target_group.target.name}"
+    }
+  }
+
+  blue_green_deployment_config {
+    deployment_ready_option {
+      action_on_timeout    = "STOP_DEPLOYMENT"
+      wait_time_in_minutes = 60
+    }
+
+    green_fleet_provisioning_option {
+      action = "COPY_AUTO_SCALING_GROUP"
+    }
+
+    terminate_blue_instances_on_deployment_success {
+      action                           = "TERMINATE"
+      termination_wait_time_in_minutes = 10
+    }
+  }*/
 }
 
 output "dns_name" {
