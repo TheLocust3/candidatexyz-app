2c7d79583ed643c500a0d6bc7ad12141ad3932fb
TheLocust3 <jake.kinsella@gmail.com>
2018-07-11 09:29:22 -0400

    Update styling of donor form

--- app/javascript/components/components/campaign/donors/DonorForm.jsx
+++ app/javascript/components/components/campaign/donors/DonorForm.jsx
@@ -19,7 +19,7 @@ export default class DonorForm extends React.Component {
         super(props);
 
         if (_.isEmpty(this.props.donor)) {
-            this.state = { donor: { }, errors: {} };
+            this.state = { donor: { dateReceived: new Date() }, errors: {} };
         } else {
             this.state = { donor: this.props.donor, errors: {} };
         }
@@ -72,27 +72,28 @@ export default class DonorForm extends React.Component {
     }
 
     render() {
+        let amount = this.state.donor.amount;
+
         return (
             <Form handleSubmit={this.handleSubmit.bind(this)} errors={this.state.errors} top>
                 <TextField label='Name' name='name' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.name} style={{ width: '100%' }} required /><br /><br />
 
-                <TextField label='Amount' name='amount' onChange={this.handleChange.bind(this)} defaultValue={String(this.state.donor.amount)} style={{ width: '100%' }} required /><br /><br />
-
                 <TextField label='Address' name='address' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.address} style={{ width: '100%' }} required /><br />
-                <TextField label='City' name='city' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.city} style={{ width: '100%' }} required /><br />
-                <TextField label='State' name='state' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.state} style={{ width: '100%' }} required /><br />
-                <TextField label='Zipcode' name='zipcode' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.zipcode} style={{ width: '100%' }} required /><br /><br /><br />
+                <TextField label='City' name='city' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.city} style={{ width: '30%', marginRight: '5%' }} required />
+                <TextField label='State' name='state' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.state} style={{ width: '30%', marginRight: '5%' }} required />
+                <TextField label='Zipcode' name='zipcode' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.zipcode} style={{ width: '30%' }} required /><br /><br />
 
-                <Text type='body2'>
+                <Text type='body2' style={{ display: 'inline-block' }}>
                     Date Received:
+
+                    <DayPickerInput inputProps={{ className: 'mdc-typography--body2' }} classNames={{ container: 'DayPickerInput mdc-typography--body2', overlayWrapper: 'DayPickerInput-OverlayWrapper mdc-typography--body2', overlay: 'DayPickerInput-Overlay mdc-typography--body2' }}
+                        formatDate={formatDate} parseDate={parseDate} value={`${formatDate(this.state.donor.dateReceived)}`} onDayChange={(date) => this.handleDateChange(date)} /><br />
                 </Text>
-                <br />
+                <TextField label='Amount' name='amount' onChange={this.handleChange.bind(this)} defaultValue={_.isEmpty(amount) ? '' : String(amount)} style={{ width: 'auto', marginLeft: '5%' }} required /><br /><br />
 
-                <DayPickerInput inputProps={{ className: 'mdc-typography--body2' }} classNames={{ container: 'DayPickerInput mdc-typography--body2', overlayWrapper: 'DayPickerInput-OverlayWrapper mdc-typography--body2', overlay: 'DayPickerInput-Overlay mdc-typography--body2' }}
-                    formatDate={formatDate} parseDate={parseDate} value={`${formatDate(this.state.donor.dateReceived)}`} onDayChange={(date) => this.handleDateChange(date)} /><br />
 
-                <TextField label='Occupation' name='occupation' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.occupation} style={{ width: '100%' }} /><br />
-                <TextField label='Employer' name='employer' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.employer} style={{ width: '100%' }} /><br /><br />
+                <TextField label='Occupation' name='occupation' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.occupation} style={{ width: '47.5%', marginRight: '5%' }} />
+                <TextField label='Employer' name='employer' onChange={this.handleChange.bind(this)} defaultValue={this.state.donor.employer} style={{ width: '47.5%' }} /><br /><br />
 
                 <Button>Save</Button>
 
